4b51fdb17e2ea0f4543b3fc619323f28ffc26e54
Sebastien Renault <srenault.contact@gmail.com>
2015-05-22 08:42:09 +0200

    On stopping demo, stop to add markers to map.

--- assets/js/cheminotm.js
+++ assets/js/cheminotm.js
@@ -2,37 +2,36 @@ var Qajax = require('qajax');
 
 exports.getStop = function(id) {
 
-  var url = Router.controllers.Cheminotm.stop(id).absoluteURL()
+  var url = Router.controllers.Cheminotm.stop(id).absoluteURL();
 
   return Qajax(url)
     .then(Qajax.filterSuccess)
     .then(function(response) {
-      return JSON.parse(response.responseText);
+      return response.responseJSON;
     });
-}
+};
 
 exports.abort = function() {
 
-  var url = Router.controllers.Cheminotm.abort().absoluteURL()
+  var url = Router.controllers.Cheminotm.abort().absoluteURL();
 
   return Qajax({
     url: url,
     method: 'POST'
   }).then(Qajax.filterSuccess)
-    .then(function(response) {
-      return JSON.parse(response.responseText);
+    .then(function() {
+      window.postMessage({
+        event: 'cheminot:abort'
+      }, window.location.origin);
     });
-}
+};
 
 exports.signout = function() {
 
-  var url = Router.controllers.Cheminotm.signout().absoluteURL()
+  var url = Router.controllers.Cheminotm.signout().absoluteURL();
 
   return Qajax({
     url: url,
     method: 'POST'
-  }).then(Qajax.filterSuccess)
-    .then(function(response) {
-      return JSON.parse(response.responseText);
-    });
-}
+  }).then(Qajax.filterSuccess);
+};

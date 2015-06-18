5b5bb626298b03829f5868a5ecac2cfd46fe4029
Sebastien Renault <srenault.contact@gmail.com>
2015-06-18 11:33:13 +0200

    Upload dist to dropbox.

--- dist.sh
+++ dist.sh
@@ -2,5 +2,5 @@
 
 sbt dist
 
-ssh sre@cheminot.org 'rm /home/sre/sites/cheminot.org/app/cheminotorg-1.0-SNAPSHOT.zip'
-scp target/universal/cheminotorg-1.0-SNAPSHOT.zip sre@cheminot.org:/home/sre/sites/cheminot.org/app/
+dropbox_uploader.sh -p upload target/universal/cheminotorg-1.0-SNAPSHOT.zip cheminotorg-latest.zip
+dropbox_uploader.sh share cheminotorg-latest.zip

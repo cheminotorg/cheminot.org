73d3032f93e856456fea85725dad37df3030575e
Sebastien Renault <srenault.contact@gmail.com>
2015-06-25 18:13:15 +0200

    Upload git version to dropbox.

--- dist.sh
+++ dist.sh
@@ -4,3 +4,6 @@ gulp --mode=prod
 sbt dist
 dropbox_uploader.sh -p upload target/universal/cheminotorg-1.0-SNAPSHOT.zip cheminotorg-latest.zip
 dropbox_uploader.sh share cheminotorg-latest.zip
+rm version
+git describe --long --always > version
+dropbox_uploader.sh -p upload version cheminotorg-version

8fddacc8b9934410fbb7eab3c500b7429309ebda
Sebastien Renault <srenault.contact@gmail.com>
2015-06-29 15:12:03 +0200

    Fix dist.sh

--- dist.sh
+++ dist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+VERSION=$(git describe --long --always)
+
 gulp --mode=prod
 sbt dist
-dropbox_uploader.sh -p upload target/universal/cheminotorg-1.0-SNAPSHOT.zip cheminotorg-latest.zip
+dropbox_uploader.sh -p upload target/universal/cheminotorg-$VERSION.zip cheminotorg-latest.zip
 dropbox_uploader.sh share cheminotorg-latest.zip
 rm version
-git describe --long --always > version
+echo $VERSION > version
 dropbox_uploader.sh -p upload version cheminotorg-version

aad76cda98f0dbfdd83a0fc4746df29e1a7d3c82
Sebastien Renault <srenault.contact@gmail.com>
2015-07-20 09:36:07 +0200

    On mobile device, check browser support for demo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var qstart = require('qstart'),
     map = require('./map'),
+    tools = require('./tools'),
     cheminotm = require('./cheminotm'),
     phone = require('./phone'),
     responsive = require('./responsive'),
@@ -11,162 +12,183 @@ responsive.init();
 
 qstart.then(function() {
 
-  var ua = window.navigator.userAgent,
-      isSafari = ua.indexOf("Safari") >= 0,
-      isChrome = ua.indexOf("Chrome") >= 0,
-      isIE = ua.indexOf("MSIE") >= 0;
-
   map.init();
 
-  if(!isIE && !(!isChrome && isSafari)) {
+  if(!tools.isMobile()) {
 
-    sessionStorage.clear();
+    if(!tools.isIE() && !(!tools.isChrome() && tools.isSafari())) {
 
-    phone.init();
+      sessionStorage.clear();
 
-    window.addEventListener("message", function(message) {
+      phone.init();
 
-      if(message.data && message.origin == window.location.origin) {
+      window.addEventListener("message", function(message) {
 
-        if(message.data.event == 'cheminot:ready') {
+        if(message.data && message.origin == window.location.origin) {
 
-          map.enableControls();
+          if(message.data.event == 'cheminot:ready') {
 
-          document.body.classList.add('playing');
+            map.enableControls();
 
-          if(!stream) {
+            document.body.classList.add('playing');
 
-            stream = Stream();
-          }
-        }
+            if(!stream) {
 
-        if(message.data.event == 'cheminot:init') {
-
-          if(message.data.error == 'full') {
-
-            phone.unavailableDemo();
-
+              stream = Stream();
+            }
           }
 
-        }
+          if(message.data.event == 'cheminot:init') {
 
-        if(message.data.event == 'cheminot:selecttrip') {
+            if(message.data.error == 'full') {
 
-          map.disableTrace();
+              phone.unavailableDemo();
 
-          map.displayTrip(message.data.trip, message.data.tdsp);
+            }
 
-        }
+          }
 
-        if(message.data.event == 'cheminot:unselecttrip') {
+          if(message.data.event == 'cheminot:selecttrip') {
 
-          map.clearTrips();
+            map.disableTrace();
 
-          map.clearTraces();
+            map.displayTrip(message.data.trip, message.data.tdsp);
 
-          map.enableTrace();
+          }
 
-        }
+          if(message.data.event == 'cheminot:unselecttrip') {
+
+            map.clearTrips();
+
+            map.clearTraces();
+
+            map.enableTrace();
+
+          }
 
 
-        if(message.data.event == 'cheminot:selectstop') {
+          if(message.data.event == 'cheminot:selectstop') {
 
-          cheminotm.getStop(message.data.stopId).then(function(stop) {
+            cheminotm.getStop(message.data.stopId).then(function(stop) {
 
-            map.addMarker(stop);
+              map.addMarker(stop);
+
+              map.fitMarkers();
+
+            });
+
+          }
+
+          if(message.data.event == 'cheminot:resetstop') {
+
+            map.removeMarker(message.data.stopId);
 
             map.fitMarkers();
 
+          }
+
+          if(message.data.event == 'cheminot:abort') {
+
+          }
+        }
+
+      });
+
+      (function() {
+
+        var androidStore = document.querySelectorAll('.android-store');
+
+        for(var i=0; i<androidStore.length; i++) {
+
+          var link = androidStore.item(i);
+
+          link.addEventListener('click', function(e) {
+
+            e.preventDefault();
+
+            analytics.trackAndroidStore().fin(function() {
+
+              window.location.href = link.getAttribute('href');
+
+            });
+
           });
-
         }
 
-        if(message.data.event == 'cheminot:resetstop') {
+      })();
 
-          map.removeMarker(message.data.stopId);
+      (function() {
 
-          map.fitMarkers();
+        var todoList = document.querySelectorAll('.roadmap input[type=checkbox]');
 
+        for (var i = 0; i < todoList.length; i++) {
+
+          var todo = todoList.item(i);
+
+          todo.addEventListener('click', function(e) {
+
+            e.preventDefault();
+
+          });
         }
 
-        if(message.data.event == 'cheminot:abort') {
+      })();
 
-        }
+      function Stream() {
+
+        var baseURL = 'http://' + Settings.domain;
+
+        var endpoint = baseURL + '/cheminotm/trace';
+
+        var stream = new EventSource(baseURL + '/cheminotm/trace');
+
+        stream.onmessage = function(msg) {
+
+          var data = JSON.parse(msg.data);
+
+          if(data) {
+
+            map.displayTrace(data);
+
+          }
+        };
+
+        stream.onerror = function(event) {
+
+          console.log(event);
+
+        };
+
+        return stream;
+      };
+
+    } else {
+
+      phone.notSupportedBrowser();
+
+    }
+  } else {
+
+    var tryDemo = document.querySelector('.try-demo');
+
+    tryDemo.addEventListener('click', function(e) {
+
+      e.preventDefault();
+
+      if(!tools.isIE() && !(!tools.isChrome() && tools.isSafari())) {
+
+        analytics.trackStartDemo().then(function() {
+
+          window.location.href = tryDemo.getAttribute('href');
+
+        });
+
+      } else {
+
+        alert('Chrome & Firefox sont les seuls navigateurs supportés par la démo.');
+
       }
 
     });
-
-    (function() {
-
-      var androidStore = document.querySelectorAll('.android-store');
-
-      for(var i=0; i<androidStore.length; i++) {
-
-        var link = androidStore.item(i);
-
-        link.addEventListener('click', function(e) {
-
-          e.preventDefault();
-
-          analytics.trackAndroidStore().fin(function() {
-
-            window.location.href = link.getAttribute('href');
-
-          });
-
-        });
-      }
-
-    })();
-
-    (function() {
-
-      var todoList = document.querySelectorAll('.roadmap input[type=checkbox]');
-
-      for (var i = 0; i < todoList.length; i++) {
-
-        var todo = todoList.item(i);
-
-        todo.addEventListener('click', function(e) {
-
-          e.preventDefault();
-
-        });
-      }
-
-    })();
-
-    function Stream() {
-
-      var baseURL = 'http://' + Settings.domain;
-
-      var endpoint = baseURL + '/cheminotm/trace';
-
-      var stream = new EventSource(baseURL + '/cheminotm/trace');
-
-      stream.onmessage = function(msg) {
-
-        var data = JSON.parse(msg.data);
-
-        if(data) {
-
-          map.displayTrace(data);
-
-        }
-      };
-
-      stream.onerror = function(event) {
-
-        console.log(event);
-
-      };
-
-      return stream;
-    };
-
-  } else {
-
-    phone.notSupportedBrowser();
-
   }
 });

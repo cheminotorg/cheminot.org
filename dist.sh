fa6cce95a80eb6100eef9324d17647f634ed012c
Sebastien Renault <srenault.contact@gmail.com>
2015-07-26 17:37:24 +0200

    Force compile with jdk 7.

--- dist.sh
+++ dist.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+function setjdk() {
+  if [ $# -ne 0 ]; then
+   removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+   if [ -n "${JAVA_HOME+x}" ]; then
+    removeFromPath $JAVA_HOME
+   fi
+   export JAVA_HOME=`/usr/libexec/java_home -v $@`
+   export PATH=$JAVA_HOME/bin:$PATH
+  fi
+  echo JAVA_HOME set to $JAVA_HOME
+  java -version
+}
+
+function removeFromPath() {
+  export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}
+
+setjdk 1.7
+
 VERSION=$(git describe --long --always)
 
 gulp --mode=prod
@@ -9,3 +28,5 @@ dropbox_uploader.sh share cheminotorg-latest.zip
 rm version
 echo $VERSION > version
 dropbox_uploader.sh -p upload version cheminotorg-version
+
+setjdk 1.8

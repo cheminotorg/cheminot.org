eebb0882ab216b0e5cdfcfe324bf357394e50aaa
Sebastien Renault <srenault.contact@gmail.com>
2016-02-28 18:49:56 +0100

    Change default password before launching tests

--- src/main/scala/storage/cypher.scala
+++ src/main/scala/storage/cypher.scala
@@ -47,6 +47,6 @@ object Cypher {
   }
 
   def withAuthentication[A](f: HttpBasicAuthentication => A): A = {
-    f(new HttpBasicAuthentication(Option("neo4j" -> "poihon")))
+    f(new HttpBasicAuthentication(Option("neo4j" -> "cheminotorg")))
   }
 }

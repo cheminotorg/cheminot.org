d7b9c74b1b8113b540c2c107fa1c39245cca411e
Sebastien Renault <srenault.contact@gmail.com>
2016-06-04 15:09:40 +0200

    Always upload latest build

--- dist/upload.sh
+++ dist/upload.sh
@@ -4,6 +4,7 @@ chmod a+x ./dist/dropbox_uploader.sh
 
 if [ -n "${TRAVIS_TAG}" ]; then
     ./dist/dropbox_uploader.sh -f ./dropbox_uploader -p upload target/*-one-jar.jar cheminotorg-${TRAVIS_TAG}.jar
-else
-    ./dist/dropbox_uploader.sh -f ./dropbox_uploader -p upload target/*-one-jar.jar cheminotorg-latest.jar
 fi
+
+./dist/dropbox_uploader.sh -f ./dropbox_uploader -p upload target/*-one-jar.jar cheminotorg-latest.jar
+

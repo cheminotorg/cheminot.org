7a1d4d5da4a27cd998ecd6fa8f245e3461d2b713
Sebastien Renault <srenault.contact@gmail.com>
2016-06-10 09:24:06 +0200

    Add configuration file

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -4,11 +4,19 @@ import scala.language.postfixOps
 import scala.concurrent.duration._
 import rapture.core._
 import rapture.cli._
-import modes.returnOption._
 import rapture.net._
-import rapture.uri._
+import rapture.json._, jsonBackends.jawn._
+import rapture.fs._
+import rapture.io._
+import rapture.codec._, encodings.`UTF-8`._
+import org.cheminot.web.log.Logger
 
-case class Config(port: Int, domain: String, mailgun: MailgunConfig, mailer: MailerConfig)
+case class Config(
+  port: Int,
+  domain: String,
+  mailgun: MailgunConfig,
+  mailer: MailerConfig
+)
 
 object Config {
 
@@ -16,13 +24,21 @@ object Config {
 
   val Domain = New.Param[String]('d', 'domain)
 
+  val ConfigFilePath = New.Param[String]('c', 'config)
+
   def apply(args: Array[String]): Config = {
+    import modes.returnOption._
     val params = New.ParamMap(args:_*)
-    val port = Port.parse(params) getOrElse 8080
-    val domain = Domain.parse(params) getOrElse "localhost:8080"
-    val mailgun = MailgunConfig("", "", uri"http://google.fr", "", "")
-    val mailer = MailerConfig(0 seconds, 10 seconds)
-    Config(port = port, domain = domain, mailgun = mailgun, mailer = mailer)
+    val configFilePath = ConfigFilePath.parse(params).map(File.parse) getOrElse ConfigFile.defaultPath
+    val configFile = ConfigFile.parse(configFilePath)
+    val port = Port.parse(params) orElse configFile.port getOrElse 8080
+    val domain = Domain.parse(params) orElse configFile.domain getOrElse "localhost:8080"
+    Config(
+      port = port,
+      domain = domain,
+      mailgun = configFile.mailgun,
+      mailer = configFile.mailer
+    )
   }
 }
 
@@ -38,3 +54,41 @@ case class MailgunConfig(
   username: String,
   password: String
 )
+
+case class ConfigFile(
+  port: Option[Int],
+  domain: Option[String],
+  mailgun: MailgunConfig,
+  mailer: MailerConfig
+)
+
+object ConfigFile {
+
+  lazy val defaultPath: FsUrl = {
+    val currentDir = new java.io.File("")
+    File.parse(s"${currentDir.getAbsolutePath}/application.json")
+  }
+
+  def parse(path: FsUrl): ConfigFile = {
+    Logger.info(s"Reading application.json ($path)")
+    val file = File.parse(path.toString)
+    val json = Json.parse(file.slurp[Char])
+    val mailgun = json.mailgun
+    val mailer = json.mailer
+    ConfigFile(
+      port = json.port.as[Option[Int]],
+      domain = json.domain.as[Option[String]],
+      mailgun = MailgunConfig(
+        from = mailgun.from.as[String],
+        to = mailgun.to.as[String],
+        endpoint = HttpQuery.parse(mailgun.endpoint.as[String]),
+        username = mailgun.username.as[String],
+        password = mailgun.password.as[String]
+      ),
+      mailer = MailerConfig(
+        delay = mailer.delay.as[Int] seconds,
+        period = mailer.delay.as[Int] seconds
+      )
+    )
+  }
+}
